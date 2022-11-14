122d1d9724603e1a93b0c4f32e91ddd1aefb8996
Ovler <ovlertheyoung@gmail.com>
2022-11-14 19:12:33 +0800

    init gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,48 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `CSSN`,
+    siteUrl: `https://ccsn.dev/`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: ["gatsby-plugin-pnpm", "gatsby-plugin-image", "gatsby-plugin-sitemap",  {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/CCSN-WebIcon.svg"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  },     {
+    resolve: '@chakra-ui/gatsby-plugin',
+    options: {
+      /**
+       * @property {boolean} [resetCSS=true]
+       * if false, this plugin will not use `<CSSReset />
+       */
+      resetCSS: true,
+      /**
+       * @property {boolean} [isUsingColorMode=true]
+       * if false, this plugin will not use <ColorModeProvider />
+       */
+      isUsingColorMode: true,
+    },
+  }]
+};
+
+export default config;
